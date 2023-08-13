4cb65eb602b55cd7afed5018d198bbadbe93034c
Sarvesh <sarvesh.w@somaiya.edu>
2023-08-13 19:37:05 +0530

    setup is done

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import {Route,Routes} from 'react-router-dom'
+
+
+export const App = () => {
+
+    return(
+
+        <>
+            <Routes>
+                <Route />
+            </Routes>
+        </>
+    )
+}
